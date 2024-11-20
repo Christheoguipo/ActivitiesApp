5cf9a8f8989194a2331b1b3e90192efe6eab1322
Theo <christheo.guipo@gmail.com>
2024-11-20 23:27:07 +0100

    Added ActivityListItem

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,54 +1,18 @@
-import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { SyntheticEvent, useState } from "react";
+import { Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import ActivityListItem from "./ActivityListItem";
 
 const ActivityList = () => {
 
   const { activityStore } = useStore();
-  const { isLoadingButton, ActivityListSortedByDate, deleteActivity } = activityStore;
-  const [target, setTarget] = useState('');
-
-  const handleDelete = (e: SyntheticEvent<HTMLButtonElement>, id: string) => {
-    setTarget(e.currentTarget.name);
-    deleteActivity(id);
-  };
+  const { ActivityListSortedByDate } = activityStore;
 
   return (
     <Segment>
       <Item.Group divided>
         {ActivityListSortedByDate.map((activity) => (
-          <Item key={activity.id}>
-            <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
-              <Item.Meta>{activity.date}</Item.Meta>
-              <Item.Description>
-                <div>{activity.description}</div>
-                <div>
-                  {activity.city}, {activity.venue}
-                </div>
-              </Item.Description>
-              <Item.Extra>
-                <Button
-                  as={Link}
-                  to={`/activities/${activity.id}`}
-                  floated="right"
-                  content="View"
-                  color="blue"
-                />
-                <Button
-                  name={activity.id}
-                  loading={isLoadingButton && target === activity.id}
-                  onClick={(e) => handleDelete(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
-                />
-                <Label basic content={activity.category} />
-              </Item.Extra>
-            </Item.Content>
-          </Item>
+          <ActivityListItem key={activity.id} activity={activity} />
         ))}
       </Item.Group>
     </Segment>
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/layout/App";
 import "./app/layout/styles.css";
 import "semantic-ui-css/semantic.min.css";
 import { store, StoreContext } from "./app/stores/store";
