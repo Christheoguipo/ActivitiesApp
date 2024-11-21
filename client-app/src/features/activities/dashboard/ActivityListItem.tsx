242f0d69f8ca25cc092bcea8808658d4bb3a2e58
Theo <christheo.guipo@gmail.com>
2024-11-21 15:49:13 +0100

    Added styling to the ActivityListItem.

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom"
-import { Button, Item, Label } from "semantic-ui-react"
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react"
 import { Activity } from "../../../app/models/activity"
 import { useStore } from "../../../app/stores/store"
 import { SyntheticEvent, useState } from "react"
+import { Link } from "react-router-dom"
 
 interface Props {
   activity: Activity
@@ -20,6 +20,40 @@ const ActivityListItem = ({ activity }: Props) => {
     deleteActivity(id);
   };
 
+
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+
+            <Item.Image size="tiny" circular src="/assets/user.png" ></Item.Image>
+            <Item.Content>
+              <Item.Header as={Link} to={`/activities/${activity.id}`} >{activity.title}</Item.Header>
+              <Item.Description>Hosted by Bob</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+
+      <Segment>
+        <Icon name="calendar" />{activity.date}
+        <span> | </span>
+        <Icon name="map marker alternate" />{activity.venue}
+      </Segment>
+
+      <Segment secondary>
+        Atendees
+      </Segment>
+
+      <Segment clearing>
+
+        <span>{activity.description}</span>
+        <Button as={Link} to={`/activities/${activity.id}`} color="teal" floated="right" content="View" />
+
+      </Segment>
+    </Segment.Group >
+  );
 
   return (
     <Item key={activity.id}>
