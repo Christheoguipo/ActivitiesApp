0c84d2aa9c0c2620897aefeb77ce069b329baa51
Theo <christheo.guipo@gmail.com>
2024-11-27 03:35:36 +0100

    Added error handling.

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using Application.Core;
+using Domain;
 using FluentValidation;
 using MediatR;
 using Persistence;
@@ -7,12 +8,12 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command: IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class CommandValidator: AbstractValidator<Command>
+        public class CommandValidator : AbstractValidator<Command>
         {
             public CommandValidator()
             {
@@ -20,7 +21,7 @@ namespace Application.Activities
             }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -28,12 +29,18 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 _context.Add(request.Activity);
 
-                await _context.SaveChangesAsync();
+                var result = await _context.SaveChangesAsync() > 0;
 
+                if (!result)
+                {
+                    return Result<Unit>.Failure("Failed to create Activity.");
+                }
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,17 +1,16 @@
-﻿using Domain;
+﻿using Application.Core;
+using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> { }
+        public class Query : IRequest<Result<List<Activity>>> { }
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
 
@@ -20,10 +19,10 @@ namespace Application.Activities
                 _context = dataContext;
             }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-               
-                return await _context.Activities.ToListAsync();
+
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
             }
         }
     }
