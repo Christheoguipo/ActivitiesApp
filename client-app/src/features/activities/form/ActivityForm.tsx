d3c7907b3b519eabe4ab8499da90a6f45ab13d14
Theo <christheo.guipo@gmail.com>
2024-12-04 21:18:09 +0100

    Added Formik.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Activity } from "../../../app/models/activity";
 import { v4 as uuid } from 'uuid';
+import { Formik } from "formik";
 
 const ActivityForm = () => {
 
@@ -28,42 +29,47 @@ const ActivityForm = () => {
     if (id) loadActivity(id).then(activity => setActivity(activity!));
   }, [id, loadActivity]);
 
-  const handleSubmit = () => {
+  // const handleSubmit = () => {
 
-    if (activity.id) {
-      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    } else {
-      activity.id = uuid();
-      createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    }
+  //   if (activity.id) {
+  //     updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+  //   } else {
+  //     activity.id = uuid();
+  //     createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+  //   }
 
-  }
+  // }
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  // const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
 
-    if (event.target.value === null) {
-      event.target.value = '';
-    }
+  //   if (event.target.value === null) {
+  //     event.target.value = '';
+  //   }
 
-    const { name, value } = event.target;
+  //   const { name, value } = event.target;
 
-    setActivity({ ...activity, [name]: value })
-  }
+  //   setActivity({ ...activity, [name]: value })
+  // }
 
   if (isLoadingList) return (<LoadingComponent content="Loading activity..." />);
 
   return (
     <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete='off' >
-        <Form.Input placeholder="Title" value={activity.title} name='title' onChange={handleInputChange} />
-        <Form.TextArea placeholder="Description" value={activity.description} name='description' onChange={handleInputChange} />
-        <Form.Input placeholder="Category" value={activity.category} name='category' onChange={handleInputChange} />
-        <Form.Input type="date" placeholder="Date" value={activity.date} name='date' onChange={handleInputChange} />
-        <Form.Input placeholder="City" value={activity.city} name='city' onChange={handleInputChange} />
-        <Form.Input placeholder="Venue" value={activity.venue} name='venue' onChange={handleInputChange} />
-        <Button loading={isLoadingButton} type="submit" floated="right" positive content="Submit" />
-        <Button as={Link} to='/activities' type="button" floated="right" content="Cancel"></Button>
-      </Form>
+      <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
+        {({ values: activity, handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit} autoComplete='off' >
+            <Form.Input placeholder="Title" value={activity.title} name='title' onChange={handleChange} />
+            <Form.TextArea placeholder="Description" value={activity.description} name='description' onChange={handleChange} />
+            <Form.Input placeholder="Category" value={activity.category} name='category' onChange={handleChange} />
+            <Form.Input type="date" placeholder="Date" value={activity.date} name='date' onChange={handleChange} />
+            <Form.Input placeholder="City" value={activity.city} name='city' onChange={handleChange} />
+            <Form.Input placeholder="Venue" value={activity.venue} name='venue' onChange={handleChange} />
+            <Button loading={isLoadingButton} type="submit" floated="right" positive content="Submit" />
+            <Button as={Link} to='/activities' type="button" floated="right" content="Cancel"></Button>
+          </Form>
+        )}
+      </Formik>
+
     </Segment>
   );
 };
