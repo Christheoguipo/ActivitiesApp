36a38f07b3d54b4f846d2ebab4380decfc6944eb
Theo <christheo.guipo@gmail.com>
2024-12-04 22:36:10 +0100

    Added reusable Text Area.

--- client-app/src/app/common/form/MyTextArea.tsx
+++ client-app/src/app/common/form/MyTextArea.tsx
@@ -4,21 +4,22 @@ import { FormField, Label } from "semantic-ui-react"
 interface Props {
   name: string;
   placeholder: string;
+  rows: number;
   label?: string;
 }
 
-const MyTextInput = (props: Props) => {
+const MyTextArea = (props: Props) => {
 
   const [field, meta] = useField(props.name);
 
   return (
     <FormField error={meta.touched && !!meta.error}>
       <label>{props.label}</label>
-      <input {...field} {...props} />
+      <textarea {...field} {...props} />
       {meta.touched && meta.error
         ? <Label basic color="red" pointing content={meta.error} /> : null}
     </FormField>
   )
 }
 
-export default MyTextInput
+export default MyTextArea
