aebdcf85ec30d98e78e7f3b9c02d02822a3f3efc
Theo <christheo.guipo@gmail.com>
2024-12-13 22:24:15 +0100

    Use token service inside AccountController.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using API.DTOs;
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly TokenService _tokenService;
 
-        public AccountController(UserManager<AppUser> userManager)
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
         {
             _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("login")]
@@ -23,13 +26,14 @@ namespace API.Controllers
             if (user == null) return Unauthorized();
 
             var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
-            if(!result) return Unauthorized();
+            if (!result) return Unauthorized();
             else
             {
-                return new UserDto {
+                return new UserDto
+                {
                     DisplayName = user.DisplayName,
                     Image = null,
-                    Token = "Token Placeholder",
+                    Token = _tokenService.CreateToken(user),
                     Username = user.UserName
                 };
             }
