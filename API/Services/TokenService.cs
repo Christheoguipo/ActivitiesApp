dbb2cc99e81200ca26f37e3635e57e565d0cd0a2
Theo <christheo.guipo@gmail.com>
2024-12-13 22:22:12 +0100

    Added TokenService.

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        public string CreateToken(AppUser appUser)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, appUser.UserName),
+                new Claim(ClaimTypes.NameIdentifier, appUser.Id),
+                new Claim(ClaimTypes.Email, appUser.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("z7nBhNVcKbqmLVtKo4s0CM88D9Tu8yuW5tM7npbqDO7PRJ32kvA8VgKIobdTUq8k"));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = credentials,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
