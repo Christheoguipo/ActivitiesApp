948957f56cd3f1f65436e6cbd9c2883d3a8ec1e2
Theo <christheo.guipo@gmail.com>
2025-01-21 22:33:09 +0100

    Feature: Add interface and implementation of adding and deleting photo using Cloudinary.

---
+++ Infrastructure/Photos/PhotosAccessor.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Photos
+{
+    public class PhotosAccessor : IPhotoAccessor
+    {
+        public Task<PhotoUploadResult> AddPhoto(IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
