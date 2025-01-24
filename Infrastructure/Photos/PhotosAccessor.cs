0378838c249ff09d8f99000cf64225b7b90f9be5
Theo <christheo.guipo@gmail.com>
2025-01-24 23:47:51 +0100

    Feature: Add logic to Photos Accessor.

--- Infrastructure/Photos/PhotosAccessor.cs
+++ Infrastructure/Photos/PhotosAccessor.cs
@@ -1,19 +1,58 @@
 ﻿using Application.Interfaces;
 using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Photos
 {
     public class PhotosAccessor : IPhotoAccessor
     {
-        public Task<PhotoUploadResult> AddPhoto(IFormFile formFile)
+        private readonly Cloudinary _cloudinary; 
+        public PhotosAccessor(IOptions<CloudinarySettings> config)
         {
-            throw new NotImplementedException();
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret);
+
+            _cloudinary = new Cloudinary(account);
+        }
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile formFile)
+        {
+            if(formFile.Length > 0)
+            {
+                await using var stream = formFile.OpenReadStream();
+
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(formFile.FileName,stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams); 
+
+                if(uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+
+                return new PhotoUploadResult
+                {
+                    PublicId = uploadResult.PublicId,
+                    Url = uploadResult.SecureUrl.ToString()
+                };
+            }
+
+            return null;
         }
 
-        public Task<string> DeletePhoto(string publicId)
+        public async Task<string> DeletePhoto(string publicId)
         {
-            throw new NotImplementedException();
+            var deleteParams = new DeletionParams(publicId);
+            var result = await _cloudinary.DestroyAsync(deleteParams);
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
