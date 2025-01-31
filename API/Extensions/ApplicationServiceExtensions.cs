ce5c00ca483ec3d9820643f37a2855d753cabe42
Theo <christheo.guipo@gmail.com>
2025-01-31 12:40:19 +0100

    Chore: Rename file.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,7 +10,7 @@ using Persistence;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtentions
+    public static class ApplicationServiceExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         { 
