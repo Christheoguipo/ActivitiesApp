a930ec8ecdffac3ce249107297930dbb705bc4a6
Theo <christheo.guipo@gmail.com>
2025-01-31 13:01:39 +0100

    Chore: Create mapping for ActivityAttendee to AttendeeDto and User to Profile.

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -5,19 +5,23 @@ using Domain;
 
 namespace Application.Core
 {
-    public class MappingProfiles: Profile
+    public class MappingProfiles : Profile
     {
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees.FirstOrDefault(a => a.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
-            .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName));
-            
+            .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+            .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(u => u.IsMain).Url));
+
             CreateMap<PhotoDto, Photo>();
+
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(d => d.Image, o => o.MapFrom(u => u.Photos.FirstOrDefault(u => u.IsMain).Url));
         }
     }
 }
