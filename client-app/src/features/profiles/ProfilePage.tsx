e99cac288e5e3bca7b5d9b0d5ff76ac9706d14d1
Theo <christheo.guipo@gmail.com>
2025-02-02 16:41:01 +0100

    feat: load and display profile to profile header and page.

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,16 +1,35 @@
 import { Grid } from 'semantic-ui-react'
 import ProfileHeader from './ProfileHeader'
 import ProfileContent from './ProfileContent'
+import { useParams } from 'react-router-dom'
+import { observer } from 'mobx-react-lite'
+import { useStore } from '../../app/stores/store'
+import { useEffect } from 'react'
+import LoadingComponent from '../../app/layout/LoadingComponent'
 
 const ProfilePage = () => {
+
+  const { username } = useParams();
+  const { profileStore } = useStore();
+  const { profile, isLoadingProfile, loadProfile } = profileStore;
+
+  useEffect(() => {
+    if (username)
+      loadProfile(username);
+  }, [loadProfile, username]);
+
+  if (isLoadingProfile)
+    return (<LoadingComponent content='Loading profile...' />)
+
   return (
     <Grid>
       <Grid.Column width={16}>
-        <ProfileHeader />
+        {profile &&
+          <ProfileHeader profile={profile} />}
         <ProfileContent />
       </Grid.Column>
     </Grid>
   )
 }
 
-export default ProfilePage
+export default observer(ProfilePage)
