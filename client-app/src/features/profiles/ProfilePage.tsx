bc78b63fc951e15b176d531a5ba867f2d6ac3e32
Theo <christheo.guipo@gmail.com>
2025-02-03 22:55:32 +0100

    feat: add profile prop to profile content.

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -25,8 +25,11 @@ const ProfilePage = () => {
     <Grid>
       <Grid.Column width={16}>
         {profile &&
-          <ProfileHeader profile={profile} />}
-        <ProfileContent />
+          <>
+            <ProfileHeader profile={profile} />
+            <ProfileContent profile={profile} />
+          </>
+        }
       </Grid.Column>
     </Grid>
   )
