af0e20a6256dcd7a82d16cef774f2193ec69e469
Theo <christheo.guipo@gmail.com>
2025-02-03 22:54:50 +0100

    feat: render profile photos when photos tab is clicked.

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,10 +1,17 @@
+import { observer } from 'mobx-react-lite'
 import { Tab, TabPane } from 'semantic-ui-react'
+import ProfilePhotos from './ProfilePhotos'
+import { Profile } from '../../app/models/profile'
 
-const ProfileContent = () => {
+interface Props {
+  profile: Profile
+}
+
+const ProfileContent = ({ profile }: Props) => {
 
   const panes = [
     { menuItem: 'About', render: () => <TabPane>About content</TabPane> },
-    { menuItem: 'Photos', render: () => <TabPane>Photos content</TabPane> },
+    { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
     { menuItem: 'Events', render: () => <TabPane>Events content</TabPane> },
     { menuItem: 'Followers', render: () => <TabPane>Followers content</TabPane> },
     { menuItem: 'Following', render: () => <TabPane>Following content</TabPane> },
@@ -15,4 +22,4 @@ const ProfileContent = () => {
   )
 }
 
-export default ProfileContent
+export default observer(ProfileContent)
