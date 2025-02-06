782e87cb128de3d73c873f3ee5479ce89ef72dae
Theo <christheo.guipo@gmail.com>
2025-02-06 16:36:16 +0100

    feat: show or hide photo upload whether the current user is the owner of the profile or not.

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,6 +1,9 @@
 import { observer } from 'mobx-react-lite'
-import { Card, Header, Image, TabPane } from 'semantic-ui-react'
+import { Button, Card, Grid, Header, Image, TabPane } from 'semantic-ui-react'
 import { Profile } from '../../app/models/profile'
+import { useStore } from '../../app/stores/store'
+import { useState } from 'react'
+import PhotoUploadWidget from '../../app/common/imageUpload/PhotoUploadWidget'
 
 interface Props {
   profile: Profile
@@ -8,16 +11,36 @@ interface Props {
 
 const ProfilePhotos = ({ profile }: Props) => {
 
+  const { profileStore: { isCurrentUser } } = useStore();
+  const [isAddPhotoMode, setIsAddPhotoMode] = useState(false);
+
   return (
     <TabPane>
-      <Header icon={'image'} content='Photos' />
-      <Card.Group itemsPerRow={5}>
-        {profile.photos?.map((photo) =>
-          <Card key={photo.id}>
-            <Image src={photo.url} />
-          </Card>
-        )}
-      </Card.Group>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated='left' icon={'image'} content='Photos' />
+          {isCurrentUser &&
+            <Button
+              floated='right'
+              basic
+              content={isAddPhotoMode ? 'Cancel' : 'Add photo'}
+              onClick={() => setIsAddPhotoMode(!isAddPhotoMode)}
+            />}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {isAddPhotoMode ? (
+            <PhotoUploadWidget />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile.photos?.map((photo) =>
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                </Card>
+              )}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
     </TabPane>
   )
 }
