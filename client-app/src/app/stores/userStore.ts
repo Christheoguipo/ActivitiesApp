254d04f7cf811dbcbd12d03baee7e9f2ed87dac6
Theo <christheo.guipo@gmail.com>
2025-02-17 23:47:44 +0100

    feat: add setDisplayName method in user store.

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -52,4 +52,9 @@ export default class UserStore {
       this.user.image = image;
   }
 
+  setDisplayName = (displayName: string) => {
+    if (this.user)
+      this.user.displayName = displayName;
+  }
+
 }
