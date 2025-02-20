7347c2e3df3e7d893d7185bf051e6cb5e72d98d2
Theo <christheo.guipo@gmail.com>
2025-02-20 23:51:32 +0100

    feat: add List handler for comments.

---
+++ Application/Comments/List.cs
@@ -0,0 +1,41 @@
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comments
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<CommentDto>>>
+        {
+            public Guid ActivityId { get; set; }
+        }
+        public class Handler : IRequestHandler<Query, Result<List<CommentDto>>>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext dataContext, IMapper mapper)
+            {
+                _dataContext = dataContext;
+                _mapper = mapper;
+            }
+
+            async Task<Result<List<CommentDto>>> IRequestHandler<Query, Result<List<CommentDto>>>.Handle(Query request, CancellationToken cancellationToken)
+            {
+
+                var comments = await _dataContext.Comments
+                    .Where(c => c.Activity.Id == request.ActivityId)
+                    .OrderBy(c => c.CreatedAt)
+                    .ProjectTo<CommentDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<CommentDto>>.Success(comments);
+            }
+        }
+
+    }
+}
