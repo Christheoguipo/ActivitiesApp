8c12823eef57998247e360413062ac443580f82c
Theo <christheo.guipo@gmail.com>
2025-02-26 22:45:44 +0100

    chore: configure CORS policy to add AllowCredentials for the SignalR to work.

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -24,7 +24,7 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
                 });
             });
 
