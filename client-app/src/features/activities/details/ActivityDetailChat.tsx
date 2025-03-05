7234e1f928b972fcc572039ae1cee6ccbff5718d
Theo <christheo.guipo@gmail.com>
2025-03-05 23:56:58 +0100

    refactor: display comment box before the comments

--- client-app/src/features/activities/details/ActivityDetailChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailChat.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Field, FieldProps, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite';
 import * as Yup from 'yup';
+import { formatDistanceToNow } from 'date-fns';
 
 interface Props {
   activitiyId: string;
@@ -30,6 +31,42 @@ const ActivityDetailChat = ({ activitiyId }: Props) => {
       </Segment>
 
       <Segment attached clearing>
+
+        <Formik
+          onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
+          initialValues={{ body: '' }}
+          validationSchema={Yup.object({
+            body: Yup.string().required()
+          })}
+        >
+          {({ isSubmitting, isValid, handleSubmit }) => (
+            <Form className='form ui' >
+              <Field name='body' >
+                {(props: FieldProps) => (
+                  <div style={{ position: 'relative' }}>
+                    <Loader active={isSubmitting} />
+                    <textarea
+                      placeholder='Enter you comment (Enter to submit, SHIFT + Enter to add new line)'
+                      {...props.field}
+                      rows={2}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter' && e.shiftKey) {
+                          return;
+                        }
+                        if (e.key === 'Enter' && !e.shiftKey) {
+                          e.preventDefault();
+                          if (isValid)
+                            handleSubmit();
+                        }
+                      }}
+                    />
+
+                  </div>
+                )}
+              </Field>
+            </Form>
+          )}
+        </Formik>
         <Comment.Group>
           {commentStore.comments.map((comment) => (
             <Comment key={comment.id}>
@@ -37,48 +74,13 @@ const ActivityDetailChat = ({ activitiyId }: Props) => {
               <Comment.Content>
                 <Comment.Author as={Link} to={`/profiles/${comment.username}`} >{comment.displayName}</Comment.Author>
                 <Comment.Metadata>
-                  <div>{comment.createdAt}</div>
+                  <div>{formatDistanceToNow(comment.createdAt)} ago</div>
                 </Comment.Metadata>
                 <Comment.Text style={{ whiteSpace: 'pre-wrap' }}>{comment.body}</Comment.Text>
               </Comment.Content>
             </Comment>
           ))}
 
-          <Formik
-            onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
-            initialValues={{ body: '' }}
-            validationSchema={Yup.object({
-              body: Yup.string().required()
-            })}
-          >
-            {({ isSubmitting, isValid, handleSubmit }) => (
-              <Form className='form ui' >
-                <Field name='body' >
-                  {(props: FieldProps) => (
-                    <div style={{ position: 'relative' }}>
-                      <Loader active={isSubmitting} />
-                      <textarea
-                        placeholder='Enter you comment (Enter to submit, SHIFT + Enter to add new line)'
-                        {...props.field}
-                        rows={2}
-                        onKeyDown={(e) => {
-                          if (e.key === 'Enter' && e.shiftKey) {
-                            return;
-                          }
-                          if (e.key === 'Enter' && !e.shiftKey) {
-                            e.preventDefault();
-                            if (isValid)
-                              handleSubmit();
-                          }
-                        }}
-                      />
-
-                    </div>
-                  )}
-                </Field>
-              </Form>
-            )}
-          </Formik>
         </Comment.Group>
       </Segment>
     </Segment.Group>
