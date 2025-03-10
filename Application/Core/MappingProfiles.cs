25ad485d360d887ac771bbfebac4196f93705a38
Theo <christheo.guipo@gmail.com>
2025-03-10 22:29:40 +0100

    chore: add properties (Following, FollowersCount and FollowingsCount) and configure automapper for Profile entity.

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -11,18 +11,23 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+
             CreateMap<Activity, ActivityDto>()
-            .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees.FirstOrDefault(a => a.IsHost).AppUser.UserName));
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees.FirstOrDefault(a => a.IsHost).AppUser.UserName));
+
             CreateMap<ActivityAttendee, AttendeeDto>()
-            .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
-            .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
-            .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-            .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(u => u.IsMain).Url));
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Bio, o => o.MapFrom(s => s.AppUser.Bio))
+                .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(u => u.IsMain).Url));
 
             CreateMap<PhotoDto, Photo>();
 
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, o => o.MapFrom(u => u.Photos.FirstOrDefault(u => u.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(u => u.Photos.FirstOrDefault(u => u.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(u => u.Followers.Count))
+                .ForMember(d => d.FollowingsCount, o => o.MapFrom(u => u.Followings.Count));
+
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(c => c.Author.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(c => c.Author.UserName))
