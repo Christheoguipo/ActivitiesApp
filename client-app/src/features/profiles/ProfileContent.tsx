c93dc2977d5a324ca24e34bae4bd1be49197e8fd
Theo <christheo.guipo@gmail.com>
2025-03-16 23:57:37 +0100

    refactor: change placeholder to ProfileFollowings and make use of setActiveTab inside of onTabChange of the Tab component.

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -3,6 +3,8 @@ import { Tab, TabPane } from 'semantic-ui-react'
 import ProfilePhotos from './ProfilePhotos'
 import { Profile } from '../../app/models/profile'
 import ProfileAbout from './ProfileAbout'
+import ProfileFollowings from './ProfileFollowings'
+import { useStore } from '../../app/stores/store'
 
 interface Props {
   profile: Profile
@@ -10,16 +12,20 @@ interface Props {
 
 const ProfileContent = ({ profile }: Props) => {
 
+  const { profileStore: { setActiveTab } } = useStore();
+
   const panes = [
     { menuItem: 'About', render: () => <ProfileAbout profile={profile} /> },
     { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
     { menuItem: 'Events', render: () => <TabPane>Events content</TabPane> },
-    { menuItem: 'Followers', render: () => <TabPane>Followers content</TabPane> },
-    { menuItem: 'Following', render: () => <TabPane>Following content</TabPane> },
+    { menuItem: 'Followers', render: () => <ProfileFollowings /> },
+    { menuItem: 'Following', render: () => <ProfileFollowings /> },
   ]
 
   return (
-    <Tab menu={{ fluid: true, vertical: true }} menuPosition='right' panes={panes} />
+    <Tab menu={{ fluid: true, vertical: true }} menuPosition='right' panes={panes}
+      onTabChange={(_, data) => setActiveTab(data.activeIndex as number)}
+    />
   )
 }
 
