4057debc41e0d4d32315a06d8ea7e19453f4a83a
Theo <christheo.guipo@gmail.com>
2025-03-19 22:52:39 +0100

    feat: make use of the profileStore's activeTab property to determine which tab the user is.

--- client-app/src/features/profiles/ProfileFollowings.tsx
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -5,13 +5,13 @@ import ProfileCard from './ProfileCard';
 
 const ProfileFollowings = () => {
   const { profileStore } = useStore();
-  const { profile, isLoadingFollowings, followings } = profileStore;
+  const { profile, isLoadingFollowings, followings, activeTab } = profileStore;
 
   return (
     <TabPane loading={isLoadingFollowings}>
       <Grid>
         <GridColumn width={16}>
-          <Header floated='left' icon={'user'} content={`People following ${profile?.displayName}`} />
+          <Header floated='left' icon={'user'} content={activeTab === 3 ? `People following ${profile?.displayName}` : `People ${profile?.displayName} is following`} />
         </GridColumn>
         <GridColumn width={16}>
           <CardGroup itemsPerRow={4}>
