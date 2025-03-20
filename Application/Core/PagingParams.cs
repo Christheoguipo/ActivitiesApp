7871a6e447ddb8286ac66ce6a8fba9c0c906a045
Theo <christheo.guipo@gmail.com>
2025-03-20 23:30:53 +0100

    feat: add PagedList and PagingParams.

---
+++ Application/Core/PagingParams.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Core
+{
+    public class PagingParams
+    {
+        public int PageNumber { get; set; } = 1;
+
+        private const int MAX_PAGE_SIZE = 50;
+        private int _pagesize = 2;
+
+        public int PageSize
+        {
+            get => _pagesize;
+            set => _pagesize = value > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : value;
+        }
+
+    }
+}
