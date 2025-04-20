7cb8215607a96e627fc253c36a2cf13ea7c43027
Theo <christheo.guipo@gmail.com>
2025-04-20 23:28:08 +0200

    feat: create paging and filtering methods.

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,10 +1,10 @@
-import { makeAutoObservable, runInAction } from "mobx"
+import { makeAutoObservable, reaction, runInAction } from "mobx"
 import { Activity, ActivityFormValues } from "../models/activity"
 import agent from "../api/agent"
 import { format } from "date-fns";
 import { store } from "./store";
 import { Profile } from "../models/profile";
-import { Pagination } from '../models/pagination';
+import { Pagination, PagingParams } from '../models/pagination';
 
 export default class ActivityStore {
 
@@ -14,9 +14,64 @@ export default class ActivityStore {
   isLoadingList: boolean = false;
   isLoadingButton: boolean = false;
   pagination: Pagination | null = null;
+  pagingParams = new PagingParams();
+  predicate = new Map().set('all', true);
 
   constructor() {
     makeAutoObservable(this);
+
+    reaction(
+      () => this.predicate.keys(),
+      () => {
+        this.pagingParams = new PagingParams();
+        this.activityList.clear();
+        this.loadActivities();
+      }
+    )
+  }
+
+  setPagingParams = (pagingParams: PagingParams) => {
+    this.pagingParams = pagingParams;
+  }
+
+  setPredicate = (predicate: string, value: string | Date) => {
+    const resetPredicate = () => {
+      this.predicate.forEach((_, key) => {
+        if (key !== 'startDate') this.predicate.delete(key);
+      })
+    }
+    switch (predicate) {
+      case 'all':
+        resetPredicate();
+        this.predicate.set('all', true);
+        break;
+      case 'isGoing':
+        resetPredicate();
+        this.predicate.set('isGoing', true);
+        break;
+      case 'isHost':
+        resetPredicate();
+        this.predicate.set('isHost', true);
+        break;
+      case 'startDate':
+        this.predicate.delete('startDate');
+        this.predicate.set('startDate', value);
+
+    }
+  }
+
+  get axiosParams() {
+    const params = new URLSearchParams();
+    params.append('pageNumber', this.pagingParams.pageNumber.toString());
+    params.append('pageSize', this.pagingParams.pageSize.toString());
+    this.predicate.forEach((value, key) => {
+      if (key === 'startDate') {
+        params.append(key, (value as Date).toISOString());
+      } else {
+        params.append(key, value);
+      }
+    })
+    return params;
   }
 
   get activityListSortedByDate() {
@@ -43,7 +98,7 @@ export default class ActivityStore {
     this.setIsLoadingList(true);
 
     try {
-      const result = await agent.Activities.list();
+      const result = await agent.Activities.list(this.axiosParams);
 
       result.data.forEach((activity) => {
         this.setActivityList(activity);
