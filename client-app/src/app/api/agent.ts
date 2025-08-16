6d17bced29850fdd60a2366dbbf349dfed492998
Christheo Guipo <christheo.guipo@gmail.com>
2025-08-16 19:07:06 +0200

    feat: add method for listActivities to agent file

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
-import { IPhoto, Profile } from '../models/profile';
+import { IPhoto, Profile, UserActivity } from '../models/profile';
 import { PaginatedResult } from '../models/pagination';
 
 const sleep = (delay: number) => {
@@ -113,7 +113,8 @@ const Profiles = {
   deletePhoto: (id: string) => requests.del(`/photos/${id}`),
   editProfile: (profile: Partial<Profile>) => requests.put('/profiles', profile),
   updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
-  listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+  listFollowings: (username: string, predicate: string) => requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`),
+  listActivities: (username: string, predicate: string) => requests.get<UserActivity[]>(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 const agent = {
