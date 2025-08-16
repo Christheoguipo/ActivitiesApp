9d97deff69f0d420f65a8136d00d54f9dbe444cd
Christheo Guipo <christheo.guipo@gmail.com>
2025-08-16 20:08:12 +0200

    feat: create component to list activities the user joined or hosted

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,10 +1,11 @@
 import { observer } from 'mobx-react-lite'
-import { Tab, TabPane } from 'semantic-ui-react'
+import { Tab } from 'semantic-ui-react'
 import ProfilePhotos from './ProfilePhotos'
 import { Profile } from '../../app/models/profile'
 import ProfileAbout from './ProfileAbout'
 import ProfileFollowings from './ProfileFollowings'
 import { useStore } from '../../app/stores/store'
+import ProfileActivities from './ProfileActivities'
 
 interface Props {
   profile: Profile
@@ -17,7 +18,7 @@ const ProfileContent = ({ profile }: Props) => {
   const panes = [
     { menuItem: 'About', render: () => <ProfileAbout profile={profile} /> },
     { menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} /> },
-    { menuItem: 'Events', render: () => <TabPane>Events content</TabPane> },
+    { menuItem: 'Events', render: () => <ProfileActivities /> },
     { menuItem: 'Followers', render: () => <ProfileFollowings /> },
     { menuItem: 'Following', render: () => <ProfileFollowings /> },
   ]
